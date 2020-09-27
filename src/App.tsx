ae26e0c00a1724f18bdb39097feb5371aab8d528
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-09-26 23:14:20 -0300

    Import global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => (
   <>
     <h1>GoBarber</h1>
     <h2>GoBarber</h2>
+    <GlobalStyle />
   </>
 );
 
