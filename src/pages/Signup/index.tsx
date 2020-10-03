a379ce7ace2319a0f44e495febbf7466fe7fb7c4
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-10-03 20:12:23 -0300

    Implements submit sended datas by used lib unform

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowDownLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -9,36 +10,38 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
   return (
-    <>
-      <Container>
-        <Background />
+    <Container>
+      <Background />
 
-        <Content>
-          <img src={logoImg} alt="GoBarber" />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
 
-          <form>
-            <h1>Faça seu logon</h1>
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
 
-            <Input name="name" icon={FiUser} placeholder="Nome" />
-            <Input name="email" icon={FiMail} placeholder="E-mail" />
-            <Input
-              name="password"
-              icon={FiLock}
-              type="password"
-              placeholder="Senha"
-            />
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
 
-            <Button type="submit">Cadastrar</Button>
-          </form>
+          <Button type="submit">Cadastrar</Button>
+        </Form>
 
-          <a href="login">
-            <FiArrowDownLeft />
-            Voltar para logon
-          </a>
-        </Content>
-      </Container>
-    </>
+        <a href="login">
+          <FiArrowDownLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
   );
 };
 
