fac984a4eea1a6db74a6610d447aaa77452fd4c1
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-10-06 00:48:37 -0300

    Add error at empty field

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -21,7 +21,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...restProps }) => {
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
 
-  const { fieldName, defaultValue, registerField } = useField(name);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
 
   const handleInputFocus = useCallback(() => {
     setIsFocused(true);
@@ -51,6 +51,8 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...restProps }) => {
         ref={inputRef}
         {...restProps}
       />
+
+      {error}
     </Container>
   );
 };
