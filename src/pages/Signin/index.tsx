8f3969f17b21f04beb8394e3de02940b88b7c056
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-10-12 04:13:43 -0300

    Ajust styles

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -10,42 +14,59 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha Obrigatória.'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
 
   return (
-    <>
-      <Container>
-        <Content>
-          <img src={logoImg} alt="GoBarber" />
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
 
-          <Form onSubmit={handleSubmit}>
-            <h1>Faça seu logon</h1>
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
 
-            <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-            <Input
-              name="password"
-              icon={FiLock}
-              type="password"
-              placeholder="Senha"
-            />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
 
-            <Button type="submit">Entrar</Button>
+          <Button type="submit">Entrar</Button>
 
-            <a href="forgot">Esqueci minha senha</a>
-          </Form>
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
 
-          <a href="login">
-            <FiLogIn />
-            Criar conta
-          </a>
-        </Content>
+        <a href="login">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
 
-        <Background />
-      </Container>
-    </>
+      <Background />
+    </Container>
   );
 };
 
