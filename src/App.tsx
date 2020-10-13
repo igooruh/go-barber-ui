d45fea455efd0f394b24bb45af8f394be349d187
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-10-13 01:47:23 -0300

    Context API

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 
+import AuthContext from './context/AuthContext';
+
 import SignIn from './pages/Signin';
 // import SignUp from './pages/Signup';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
-    {/* <SignUp /> */}
+    <AuthContext.Provider value={{ name: 'Ígor' }}>
+      <SignIn />
+      {/* <SignUp /> */}
+    </AuthContext.Provider>
     <GlobalStyle />
   </>
 );
