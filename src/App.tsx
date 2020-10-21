6aace74e8d599b9cecb3e6d7d886a9ee33206049
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-10-21 00:52:02 -0300

    Remove import. Add hooks providers

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,17 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './hooks/AuthContext';
-import Toast from './components/ToastContainer';
+import AppProvider from './hooks';
 
 import SignIn from './pages/Signin';
 // import SignUp from './pages/Signup';
 
 const App: React.FC = () => (
   <>
-    <AuthProvider>
+    <AppProvider>
       <SignIn />
       {/* <SignUp /> */}
-    </AuthProvider>
-
-    <Toast />
+    </AppProvider>
 
     <GlobalStyle />
   </>
