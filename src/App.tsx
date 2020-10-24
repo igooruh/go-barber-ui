b18fa312c86b0998b38b1618b3d7ebddac6df7d1
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-10-24 17:52:36 -0300

    Config routes and animations pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 
 import AppProvider from './hooks';
 
-import SignIn from './pages/Signin';
-// import SignUp from './pages/Signup';
+import Routes from './routes';
 
 const App: React.FC = () => (
-  <>
+  <Router>
     <AppProvider>
-      <SignIn />
-      {/* <SignUp /> */}
+      <Routes />
     </AppProvider>
 
     <GlobalStyle />
-  </>
+  </Router>
 );
 
 export default App;
